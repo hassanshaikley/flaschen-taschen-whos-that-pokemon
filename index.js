73387af91cc80f789a3b8fb0ac0c6b93f07c9cef
Hassan Shaikley <hassan.shaikley@gmail.com>
2016-03-20 16:27:42 -0700

    Added socket io support

--- index.js
+++ index.js
@@ -2,29 +2,33 @@ var util = require('util');
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io').listen(http);
+
 var Game = require('./game').Game;
 
-var app = express();
 
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
 app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/lib/views'));
+
+
 
 app.get('/', function (req, res) {
     res.render('index.html');
 });
 
 app.post('/guess', function (req, res) {
-    console.log("Guess received: "+req.body.pokemon);
-    app.game.checkPokemon(req.body.pokemon);
     res.redirect('/');
 });
 
-app.listen(3000, function () {
+http.listen(3000, function () {
     console.log('Who\'s that Pokemon listening on port 3000!');
     app.game = new Game();
     var args = process.argv.slice(2);
@@ -32,4 +36,17 @@ app.listen(3000, function () {
         app.game.target = args[0];
     }
     app.game.start();
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('guess', function(data){
+        console.log("Guess received: "+data.guess);
+        app.game.checkPokemon(data.guess);
+        this.emit('guess', { guess: data.guess });
+        this.broadcast.emit('guess', { guess: data.guess });
+    });
 });
