2c70bff51bae987c5996512412d4d16acec285d9
Hassan Shaikley <hassan.shaikley@gmail.com>
2016-03-20 02:07:16 -0700

    Randomly generates pokemon with proper id; also empty webapp

---
+++ index.js
@@ -0,0 +1,44 @@
+var exec = require('child_process').exec;
+var util = require('util')
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+
+
+/* Function for printing std info */
+function puts(error, stdout, stderr) { console.log(stdout) }
+
+function Game(){
+  this.current_pokemon;
+  this.already_asked = [];
+}
+
+Game.prototype.newPokemon = function(){
+  //if generation == 151
+  var num = (Math.floor(Math.random()*150 + 0)) // number between 0 and 150
+  var correct_pokemon = num+1; 
+  if (this.already_asked.indexOf(correct_pokemon) != -1){
+    this.newPokemon();
+  }
+    //for num 50 the correct answer is 51
+  var pokemon_file =  "images/pokemon_" + num +".png";
+  var str = "./../flaschen-taschen/client/send-image -h localhost -g 32x32-3-3 ";
+  str+=pokemon_file;
+  exec(str, puts);
+  console.log("Pokemon num is " + correct_pokemon);
+
+  this.already_asked.push(correct_pokemon);
+}
+
+var game = new Game();
+game.newPokemon();
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
